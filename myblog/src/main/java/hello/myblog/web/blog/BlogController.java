492843a3c7f2265200c83209b2fe06c0a7e4a667
YeongJae0114 <yjl8628@naver.com>
2024-02-23 15:40:45 +0900

    요청로직 구현 Controller

--- myblog/src/main/java/hello/myblog/web/blog/BlogController.java
+++ myblog/src/main/java/hello/myblog/web/blog/BlogController.java
@@ -2,19 +2,24 @@ package hello.myblog.web.blog;
 
 import hello.myblog.domain.blog.Blog;
 import hello.myblog.domain.blog.BlogRepository;
+import hello.myblog.web.blog.form.BlogSaveForm;
+import hello.myblog.web.blog.form.BlogUpdateForm;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/blogs")
 @RequiredArgsConstructor
+@Slf4j
 public class BlogController {
     private final BlogRepository blogRepository;
 
@@ -29,6 +34,56 @@ public class BlogController {
         Blog blog = blogRepository.findById(blogId);
         model.addAttribute("blog",blog);
         return "blogs/blog";
+    }
+
+    @GetMapping("/{blogId}/edit")
+    public String editForm(@PathVariable Long blogId, Model model){
+        Blog blog = blogRepository.findById(blogId);
+        model.addAttribute("blog",blog);
+        return "blogs/editForm";
+    }
+    @PostMapping("/{blogId}/edit")
+    public String edit(@PathVariable Long blogId, @Validated @ModelAttribute("blog")BlogUpdateForm form, BindingResult bindingResult){
+        if (form.getTitle() == null && form.getContent() == null ) {
+            bindingResult.reject("err");
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "blogs/editForm";
+        }
+        Blog blog = new Blog();
+        blog.setTitle(form.getTitle());
+        blog.setContent(form.getContent());
+
+        blogRepository.update(blogId, blog);
+        return "redirect:/blogs/{blogId}";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model){
+        model.addAttribute(new Blog());
+        return "blogs/addForm";
+    }
+    @PostMapping("/add")
+    public String addBlog(@Validated @ModelAttribute("blog")BlogSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+        if (form.getTitle() == null && form.getContent() == null ){
+
+        }
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "blogs/addForm";
+        }
+        // 성공
+        Blog blog = new Blog();
+        blog.setTitle(form.getTitle());
+        blog.setContent(form.getContent());
+
+        Blog saveBlog = blogRepository.save(blog);
+        redirectAttributes.addAttribute("blogId", saveBlog.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/blogs/{blogId}";
+
     }
 
     @PostConstruct
